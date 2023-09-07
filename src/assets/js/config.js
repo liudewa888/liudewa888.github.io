9ac829cf5cf5467a5f984fa23c58e9fe0253e962
liudewa888 <2549711475@qq.com>
2023-09-07 23:15:33 +0800

    新增:百度GPT

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,209 +1,215 @@
-
 // 前端网址
 export const navigations = [
   {
-    category: '搜索',
+    category: "搜索",
     children: [
       {
-        title: 'MDN',
-        link: 'https://developer.mozilla.org/zh-CN/'
+        title: "MDN",
+        link: "https://developer.mozilla.org/zh-CN/",
       },
       {
-        title: 'GitHub',
-        link: 'https://github.com/search?q='
+        title: "GitHub",
+        link: "https://github.com/search?q=",
       },
       {
-        title: 'NPM',
-        link: 'https://www.npmjs.com/'
+        title: "NPM",
+        link: "https://www.npmjs.com/",
       },
       {
-        title: 'stackoverflow',
-        link: 'https://stackoverflow.com/search?q='
+        title: "stackoverflow",
+        link: "https://stackoverflow.com/search?q=",
       },
       {
-        title: '开发者搜索',
-        link: 'https://kaifa.baidu.com/'
+        title: "开发者搜索",
+        link: "https://kaifa.baidu.com/",
       },
       {
-        title: 'Goobe',
-        link: 'https://goobe.io/'
+        title: "Goobe",
+        link: "https://goobe.io/",
       },
-    ]
+    ],
   },
   {
-    category: '文档',
+    category: "文档",
     children: [
       {
-        title: 'Vue',
-        link: 'https://cn.vuejs.org/api/'
+        title: "Vue",
+        link: "https://cn.vuejs.org/api/",
       },
       {
-        title: 'NodeJS',
-        link: 'https://nodejs.org/docs/latest-v15.x/api/'
+        title: "NodeJS",
+        link: "https://nodejs.org/docs/latest-v15.x/api/",
       },
       {
-        title: '网道',
-        link: 'https://wangdoc.com/'
+        title: "网道",
+        link: "https://wangdoc.com/",
       },
       {
-        title: 'TS',
-        link: 'https://www.typescriptlang.org/docs/'
+        title: "TS",
+        link: "https://www.typescriptlang.org/docs/",
       },
       {
-        title: 'Webpack',
-        link: 'https://yk2012.github.io/sgg_webpack5/base/optimizeCss.html#_2-%E9%85%8D%E7%BD%AE'
-      }
-    ]
+        title: "Webpack",
+        link: "https://yk2012.github.io/sgg_webpack5/base/optimizeCss.html#_2-%E9%85%8D%E7%BD%AE",
+      },
+    ],
   },
   {
-    category: '工具',
+    category: "工具",
     children: [
       {
-        title: 'DeepL翻译',
-        link: 'https://www.deepl.com/translator'
+        title: "DeepL翻译",
+        link: "https://www.deepl.com/translator",
       },
       {
-        title: '百度翻译',
-        link: 'https://fanyi.baidu.com/'
+        title: "百度翻译",
+        link: "https://fanyi.baidu.com/",
       },
       {
-        title: '星火GPT',
-        link: 'https://xinghuo.xfyun.cn/desk'
+        title: "谷歌翻译",
+        link: "https://translate.google.com/",
       },
-    ]
+      {
+        title: "星火GPT",
+        link: "https://xinghuo.xfyun.cn/desk",
+      },
+      {
+        title: "百度GPT",
+        link: "https://yiyan.baidu.com/",
+      },
+    ],
   },
   {
-    category: '技术',
+    category: "技术",
     children: [
       {
-        title: '稀土掘金',
-        link: 'https://juejin.cn/'
+        title: "稀土掘金",
+        link: "https://juejin.cn/",
       },
       {
-        title: '吾爱破解',
-        link: 'https://www.52pojie.cn/'
+        title: "吾爱破解",
+        link: "https://www.52pojie.cn/",
       },
       {
-        title: '思否',
-        link: 'https://segmentfault.com/'
+        title: "思否",
+        link: "https://segmentfault.com/",
       },
       {
-        title: 'GitChat',
-        link: 'https://gitbook.cn/chat/'
+        title: "GitChat",
+        link: "https://gitbook.cn/chat/",
       },
       {
-        title: 'V2EX',
-        link: 'https://www.v2ex.com/'
+        title: "V2EX",
+        link: "https://www.v2ex.com/",
       },
       {
-        title: 'fly63',
-        link: 'https://www.fly63.com/'
+        title: "fly63",
+        link: "https://www.fly63.com/",
       },
-    ]
+    ],
   },
   {
-    category: '博客',
+    category: "博客",
     children: [
       {
-        title: '阮一峰',
-        link: 'http://ruanyifeng.com/'
+        title: "阮一峰",
+        link: "http://ruanyifeng.com/",
       },
       {
-        title: '每日 Github',
-        link: 'https://github.com/Wechat-ggGitHub/Awesome-GitHub-Repo'
+        title: "每日 Github",
+        link: "https://github.com/Wechat-ggGitHub/Awesome-GitHub-Repo",
       },
-    ]
+    ],
   },
   {
-    category: '其它',
+    category: "其它",
     children: [
       {
-        title: '搜索技巧',
-        link: '/search'
+        title: "搜索技巧",
+        link: "/search",
       },
       {
-        title: '快捷和命令',
-        link: '/command'
+        title: "快捷和命令",
+        link: "/command",
       },
-    ]
+    ],
   },
-]
+];
 // 搜索引擎
 export const searchs = [
   {
-    name: '谷 歌',
-    link: 'https://www.google.com/search',
-    key: 'q',
+    name: "谷 歌",
+    link: "https://www.google.com/search",
+    key: "q",
   },
   {
-    name: '必 应',
-    link: 'https://www.bing.com/search',
-    key: 'q',
+    name: "必 应",
+    link: "https://www.bing.com/search",
+    key: "q",
   },
   {
-    name: 'duck',
-    link: 'https://duckduckgo.com/',
-    key: 'q',
+    name: "duck",
+    link: "https://duckduckgo.com/",
+    key: "q",
   },
   {
-    name: '百 度',
-    link: 'https://www.baidu.com/s',
-    key: 'wd',
+    name: "百 度",
+    link: "https://www.baidu.com/s",
+    key: "wd",
   },
   {
-    name: 'F 搜',
-    link: 'https://fsoufsou.com/search',
-    key: 'q',
+    name: "F 搜",
+    link: "https://fsoufsou.com/search",
+    key: "q",
   },
-]
+];
 // 包含网址
 export const includes = [
   {
-    title: '掘金',
-    link: 'juejin.cn',
+    title: "掘金",
+    link: "juejin.cn",
   },
   {
-    title: '知乎',
-    link: 'zhihu.com',
+    title: "知乎",
+    link: "zhihu.com",
   },
   {
-    title: '吾爱',
-    link: '52pojie.cn',
+    title: "吾爱",
+    link: "52pojie.cn",
   },
   {
-    title: '思否',
-    link: 'segmentfault.com',
+    title: "思否",
+    link: "segmentfault.com",
   },
   {
-    title: 'stackoverflow',
-    link: 'stackoverflow.com',
+    title: "stackoverflow",
+    link: "stackoverflow.com",
   },
   {
-    title: 'GitHub',
-    link: 'github.com',
+    title: "GitHub",
+    link: "github.com",
   },
-]
+];
 // 排除网址
 export const excludes = [
   {
-    title: 'CSDN',
-    link: 'csdn.net'
+    title: "CSDN",
+    link: "csdn.net",
   },
   {
-    title: '脚本之家',
-    link: 'jb51.net'
+    title: "脚本之家",
+    link: "jb51.net",
   },
   {
-    title: '腾讯云',
-    link: 'cloud.tencent.com'
+    title: "腾讯云",
+    link: "cloud.tencent.com",
   },
   {
-    title: '华为云',
-    link: 'huaweicloud.com'
+    title: "华为云",
+    link: "huaweicloud.com",
   },
   {
-    title: '阿里云',
-    link: 'developer.aliyun.com'
+    title: "阿里云",
+    link: "developer.aliyun.com",
   },
-]
-
+];
